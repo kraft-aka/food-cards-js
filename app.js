4c1cb1871ed6a647e086c7a03b04848cee43fc6d
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-10-15 16:17:43 +0200

    update function

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const slides = document.querySelectorAll(".slide");
+function CardSlider( sliderNum=0 ) {
+  const slides = document.querySelectorAll(".slide");
 
-slides.forEach((slide) => {
-  slide.addEventListener("click", () => {
-    clearActiveClasses();
-    slide.classList.add("active");
+  slides[sliderNum].classList.add("active");
+
+  slides.forEach((slide) => {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
   });
-});
 
-const clearActiveClasses = () =>
-  slides.forEach((slide) => slide.classList.remove("active"));
+  const clearActiveClasses = () =>
+    slides.forEach((slide) => slide.classList.remove("active"));
+}
+
+CardSlider(3)
